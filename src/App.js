815fdd89e3f85a2bc18bd37c2f29d8e963c405b9
Ayesha Zahid <47062178+AishaZahid21@users.noreply.github.com>
2020-11-21 04:06:50 +0500

    add all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 // import AccountSummary from './components/AccountSummary';
-import Header from "./components/header"
+import Header from "./components/Header"
+import Balance from "./components/Balance"
+import AccountSummary from "./components/AccountSummary"
+import TransactionHistory from "./components/TransactionHistory"
+import AddTransaction from "./components/AddTransaction"
 
 import  { GlobalProvider } from "./context/GlobalState"
 function App() {
@@ -9,10 +13,10 @@ function App() {
     <div>
       <Header />
       <div className="container"></div>
-      {/* <Balance /> */}
-      {/* <AccountSummary /> */}
-      {/* <TransactionHistory /> */}
-      {/* <AddTransaction /> */}
+      <Balance />
+      <AccountSummary />
+      <TransactionHistory />
+      <AddTransaction />
       </div>
       </GlobalProvider>
   );
